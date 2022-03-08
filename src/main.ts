4d8c5e642a4c4a113529894305a8f52f5c1f3433
Stephan Spengler <stephanspengler93@gmail.com>
2022-03-08 21:19:09 +0100

    beta version 2.0.0-b1

--- src/main.ts
+++ src/main.ts
@@ -16,7 +16,7 @@ import MainWindow from "./window/MainWindow";
 
 registerPlugin({
     name: "scenery-manager",
-    version: "2.0.0-b0",
+    version: "2.0.0-b1",
     authors: ["Sadret"],
     type: "local",
     licence: "GPL-3.0",
