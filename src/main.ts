eb55c22092b07155774da64fc4f36e4e87a69f49
Stephan Spengler <stephanspengler93@gmail.com>
2023-07-05 17:54:49 +0200

    v2.0.3: fix new track elements copy, paste, mirror

--- src/main.ts
+++ src/main.ts
@@ -16,7 +16,7 @@ import MainWindow from "./window/MainWindow";
 
 registerPlugin({
     name: "scenery-manager",
-    version: "2.0.2",
+    version: "2.0.3",
     authors: ["Sadret"],
     type: "local",
     licence: "GPL-3.0",
